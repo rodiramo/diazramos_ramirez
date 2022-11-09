b7723ee93083538351ce3635f89efcf5837c6c24
rodiramo <82394566+rodiramo@users.noreply.github.com>
2022-11-09 02:10:11 +0100

    Update home.php

--- vistas/home.php
+++ vistas/home.php
@@ -1,6 +1,9 @@
 <?php
 require_once __DIR__ . '/../libraries/products.php';
-$products = productosTodo(); ?>
+$products = (new Product)->todo(); 
+
+
+?>
 
 <section id="divider-home" style="height: 10rem;">
     <div class="block-color"></div>
@@ -8,7 +11,6 @@ $products = productosTodo(); ?>
     <div class="block-color3"></div>
     <div class="block-color4"></div>
     <h2 class="text1">The best shop for buying plants</h2>
-    <h2 class="text2">Make your House a Home with Blomma.</h2>
     <div class="linea2"></div>
     <p>Touching the hearts of all Plants lovers</p>
 </section>
@@ -17,7 +19,8 @@ $products = productosTodo(); ?>
 <!-- carousel -->
 <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-indicators">
-        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true"
+            aria-label="Slide 1"></button>
         <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
         <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
@@ -50,88 +53,99 @@ $products = productosTodo(); ?>
 <section class="species-infocard">
     <h2>Our Species</h2>
     <div class="line-species"></div>
-    <p>Plants come from different families and theres different ways to take care of them, here we tell you a bit about them so you can take the best care of your plants!</p>
+    <p>Plants come from different families and theres different ways to take care of them, here we tell you a bit about
+        them so you can take the best care of your plants!</p>
 
 
 
     <ul class="cards-tipos">
         <li>
             <a href="" class="card-tipos">
-                <img src="https://images.pexels.com/photos/4622876/pexels-photo-4622876.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" class="card__image" alt="anthurium" />
+                <img src="https://images.pexels.com/photos/4622876/pexels-photo-4622876.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                    class="card__image" alt="anthurium" />
                 <div class="card__overlay">
                     <div class="card__header">
                         <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
-                         
+
                         </svg>
                         <div class="card__header-text">
                             <h3 class="card__title">Anthurium</h3>
                         </div>
                     </div>
-                    <p class="card__description">The anthurium is not only a plant that is very easy to care for, but many species of anthurium are among the most beautiful and unusual houseplants that exist.</p>
+                    <p class="card__description">The anthurium is not only a plant that is very easy to care for, but
+                        many species of anthurium are among the most beautiful and unusual houseplants that exist.</p>
                 </div>
             </a>
         </li>
         <li>
             <a href="" class="card-tipos">
-                <img src="https://images.pexels.com/photos/4870767/pexels-photo-4870767.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" class="card__image" alt="philodendrons" />
+                <img src="https://images.pexels.com/photos/4870767/pexels-photo-4870767.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                    class="card__image" alt="philodendrons" />
                 <div class="card__overlay">
                     <div class="card__header">
                         <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
-                            
+
                         </svg>
                         <div class="card__header-text">
                             <h3 class="card__title">Philodendrons</h3>
                         </div>
                     </div>
-                    <p class="card__description">Philodendrons are so popular because they possess air-purifying properties and are not difficult to maintain.</p>
+                    <p class="card__description">Philodendrons are so popular because they possess air-purifying
+                        properties and are not difficult to maintain.</p>
                 </div>
             </a>
         </li>
         <li>
             <a href="" class="card-tipos">
-                <img src="https://images.pexels.com/photos/6805805/pexels-photo-6805805.jpeg" class="card__image" alt="alocasia" />
+                <img src="https://images.pexels.com/photos/6805805/pexels-photo-6805805.jpeg" class="card__image"
+                    alt="alocasia" />
                 <div class="card__overlay">
                     <div class="card__header">
                         <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
-                         
+
                         </svg>
                         <div class="card__header-text">
                             <h3 class="card__title">Alocasia</h3>
                         </div>
                     </div>
-                    <p class="card__description">The Alocasia is a houseplant that is not so easily overlooked. This is due to their spectacular foliage and visual attractiveness </p>
+                    <p class="card__description">The Alocasia is a houseplant that is not so easily overlooked. This is
+                        due to their spectacular foliage and visual attractiveness </p>
                 </div>
             </a>
         </li>
         <li>
             <a href="" class="card-tipos">
-                <img src="https://images.pexels.com/photos/4593961/pexels-photo-4593961.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" class="card__image" alt="calathea" />
+                <img src="https://images.pexels.com/photos/4593961/pexels-photo-4593961.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                    class="card__image" alt="calathea" />
                 <div class="card__overlay">
                     <div class="card__header">
                         <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
-                       
+
                         </svg>
                         <div class="card__header-text">
                             <h3 class="card__title">Calathea</h3>
                         </div>
                     </div>
-                    <p class="card__description">The Calathea is a tropical plant with colorful foliage. It is easy to care for and has excellent air-purifying properties.</p>
+                    <p class="card__description">The Calathea is a tropical plant with colorful foliage. It is easy to
+                        care for and has excellent air-purifying properties.</p>
                 </div>
             </a>
         </li>
         <li>
             <a href="" class="card-tipos">
-                <img src="https://images.pexels.com/photos/1407305/pexels-photo-1407305.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" class="card__image" alt="monstera" />
+                <img src="https://images.pexels.com/photos/1407305/pexels-photo-1407305.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                    class="card__image" alt="monstera" />
                 <div class="card__overlay">
                     <div class="card__header">
                         <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
-                       
+
                         </svg>
                         <div class="card__header-text">
                             <h3 class="card__title">Monstera</h3>
                         </div>
                     </div>
-                    <p class="card__description">The Monstera is a tropical plant and has leaves that can grow monstrously large. The Monstera plant is an excellent air purifier.</p>
+                    <p class="card__description">The Monstera is a tropical plant and has leaves that can grow
+                        monstrously large. The Monstera plant is an excellent air purifier.</p>
                 </div>
             </a>
         </li>
@@ -139,7 +153,7 @@ $products = productosTodo(); ?>
 
 
 
-   
+
 </section>
 
 <!--product cards-->
@@ -158,26 +172,28 @@ $products = productosTodo(); ?>
 
         <div class="cards-container">
             <?php foreach ($products as $product) : ?>
-                <div class="cards-item">
+            <div class="cards-item">
 
-                    <div class="card">
-                        <div class="card_image"><img src="imgs/producto-<?= $product->product_img; ?>" alt="<?= $product->product_name ?>"></div>
-                        <div class="card_content">
-                            <em class="product_category"><?= $product->product_category; ?></em>
-                            <h2 class="card_title"><?= $product->product_name ?></h2>
-                            <p><?= $product->product_type; ?></p>
-                            <p class="card_text"><?= $product->product_price; ?></p>
+                <div class="card">
+                    <div class="card_image"><img src="imgs/producto-<?= $product->getImg(); ?>"
+                            alt="<?= $product->getTitulo() ?>"></div>
+                    <div class="card_content">
+                        <em class="product_category"><?= $product->getCategory(); ?></em>
+                        <h2 class="card_title"><?= $product->getTitulo() ?></h2>
+                        <p><?= $product->getType(); ?></p>
+                        <p class="card_text"><?= $product->getPrice(); ?></p>
 
-                            <div class="cart-card"> <a href="#" id="add-to-cart"><i class='bx bx-cart'></i></a></div>
-                        </div>
-                        <div class="inside">
-                            <div class="icon">
-                                <a href="index.php?s=product-view&id=<?= $product->product_id; ?>"><i class='bx bx-info-circle'></i></a>
-                            </div>
+                        <div class="cart-card"> <a href="#"><i class='bx bx-cart'></i></a></div>
+                    </div>
+                    <div class="inside">
+                        <div class="icon">
+                            <a href="index.php?s=product-view&id=<?= $product->getProductId() ?>"><i
+                                    class='bx bx-info-circle'></i></a>
                         </div>
                     </div>
-
                 </div>
+
+            </div>
             <?php endforeach; ?>
 
         </div>
